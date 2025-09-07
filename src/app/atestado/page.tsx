403031459bad58852eb8a018b68c2a385859db4f
drnewtonlauer <newtonlauer@gmail.com>
2025-09-07 19:32:45 -0300

    Updated page.tsx

--- src/app/atestado/page.tsx
+++ src/app/atestado/page.tsx
@@ -1,15 +1,14 @@
-@@ .. @@
- import { Metadata } from 'next';
- 
- export const metadata: Metadata = {
-   title: 'Atestados - Med Lauer',
-   description: 'Emissão de atestados médicos',
--  viewport: 'width=device-width, initial-scale=1',
- };
- 
-+export const viewport = {
-+  width: 'device-width',
-+  initialScale: 1,
-+};
-+
- export default function AtestadoPage() {
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Atestados - Med Lauer',
+  description: 'Emissão de atestados médicos',
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
+
+export default function AtestadoPage() {
+}
