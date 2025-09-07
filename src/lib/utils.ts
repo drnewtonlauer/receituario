729566c50f1193c40facd0e5829a7e8e288d8b78
drnewtonlauer <newtonlauer@gmail.com>
2025-09-07 18:39:46 -0300

    Updated utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,10 @@
-@@ .. @@
- import { clsx, type ClassValue } from 'clsx'
--import { twMerge } from 'tailwindcss-merge'
-+import { twMerge } from 'tailwind-merge'
- 
- export function cn(...inputs: ClassValue[]) {
-   return twMerge(clsx(inputs))
- }
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Junta classes do Tailwind sem duplicação/confli to,
+ * combinando clsx + tailwind-merge.
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
